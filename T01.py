d0255ccec7cb9eb2bf6b7e4dd1faebd903cf0561
Omer Eisenberg <omereis@yahoo.com>
2018-01-04 13:56:32 -0500

    removed unnecessary files

--- T01.py
+++ T01.py
@@ -2,34 +2,21 @@
 
 import ast
 import inspect
+import math
 print ("Hello, Python!")
-t1 = ast.parse("q = a + b / 2")
-t2 = ast.parse("q = (a + b) / 2")
-f = t1 == t2
 
-print ("Where can I see this string?")
-print('Here\'s a string with a single "\'"')
-print ("Here's a comment: \n#comment")
-name = "Moses"
-print ("'name' is '" + name + "'")
-len = 4 + 3j
-print (abs(len))
-dicti = {'1': "one", '2': "two", '3': "three"}
+def get_g (L, T, th):
+    theta = th * math.pi / 180.0
+    par = (1 + 1.0 / 4.0 * math.sin(theta/2)**2)
+    return ((4.0 * (math.pi ** 2) * L / T ** 2) * par)
 
-print ('dictionaty of 1 is: ' + dicti['1'])
-print("\ndictionary values:")
-print(dicti.values())
-print("\ndictionary keys:")
-print(dicti.keys())
-#print("dictionary:\n" + dicti)
-#print("Keys:\n" + dicti.keys())
-#print("values:\n" + dicti.values())
-city_population = {"New York City":8550405, "Los Angeles":3971883, "Toronto":2731571, "Chicago":2720546, "Houston":2296224, "Montreal":1704694, "Calgary":1239220, "Vancouver":631486, "Boston":667137}
-city_p = {"New York City":'8550405', "Los Angeles":'3971883', "Toronto":'2731571'}
-print("Pop. of Toronto:")
-print(city_population["Toronto"])
-print(city_p["Toronto"])
+if __name__ == "__main__":
+    L=0.5
+    T=1.443
+    th=30
 
-print("\n")
-print (city_population)
-print (city_p)
+    g = get_g(0.5, 1.443, 30)
+    g1 = get_g(0.5-0.05,1.443+0.02,30-5)
+    print("g (0.5,1.443,30) = " + str(g))
+    print("g (0.5-0.05,1.443+0.02,30-5) = " + str(g1))
+    print("delta(g)/g = " + str((g - g1) / g))

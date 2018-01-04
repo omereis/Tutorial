73c033e1bbab4175d6db9c80d78904cc5438b3d3
Omer Eisenberg <omereis@yahoo.com>
2018-01-04 14:02:02 -0500

    changes

--- T01.py
+++ T01.py
@@ -20,3 +20,20 @@ if __name__ == "__main__":
     print("g (0.5,1.443,30) = " + str(g))
     print("g (0.5-0.05,1.443+0.02,30-5) = " + str(g1))
     print("delta(g)/g = " + str((g - g1) / g))
+    try :
+        print("Format 2 params: %s = %s" % ("one","two"))
+        g_name = "rg"
+        str1 = "Default Parameters are unknown to C: '"
+        val = 60
+        print ("Default Parameters are unknown to C: '%s = %s'" % (g_name, str(val)))
+        arg_name = g_name
+        w_str = ("Default Parameters are unknown to C: '%s = %s'" % (arg_name, str(val)))
+        print (w_str)
+        print ("Default Parameters are unknown to C: '" + g_name + "=")
+        print ("Default Parameters are unknown to C: '" + g_name + " = " )#+ str(60) + "'")
+        val += 1
+#        w_str = ("Default Parameters are unknown to C:")# '%s = %s'" % (arg_name, "60"))
+#        print (w_str)
+#       w_str += "OK"
+    except Exception as excp:
+        print ("Error:\n" + str(excp.args))

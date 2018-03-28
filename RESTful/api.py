acef04b2a6206881c9f6c7ba3c15f6f0f6190a0d
Omer Eisenberg <omereis@yahoo.com>
2018-03-28 08:48:10 -0400

    added slurm folder

--- RESTful/api.py
+++ RESTful/api.py
@@ -11,18 +11,27 @@ debug_file = 'd:\\omer\\source\\tutorial\\RESTful\\debug.txt'
 class TodoSimple (Resource):
     def get(self,todo_id):
         try:
+            return_value = {todo_id: todos[todo_id]}
+        except:
+            return_value = str(todo_id) + ": no such value"
+        try:
             f = open(debug_file, 'a')
             f.write("-----------------------------\n")
             f.write("api.py-TodoSimple.get()\n")
             f.write("todo_id=" + str(todo_id) + "\n")
             f.write("todos =" + str(todos) + "\n")
-            f.write("todos[todo_id]=" + str(todos[todo_id]) + "\n")
+            f.write("todos[todo_id]=" + str(return_value) + "\n")
         finally:
             f.close()
-        return {todo_id:todos[todo_id]}
+        return {todo_id: return_value}
+#        return {todo_id:todos[todo_id]}
 
     def put(self, todo_id):
         todos[todo_id] = request.form['data']
+        try:
+            return_value = {todo_id: todos[todo_id]}
+        except:
+            return_value = str(todo_id) + ": no such value"
         try:
             f = open(debug_file, 'a')
             f.write("-----------------------------\n")
@@ -32,9 +41,11 @@ class TodoSimple (Resource):
             f.write("todos[todo_id]=" + str(todos[todo_id]) + "\n")
         finally:
             f.close()
-        return {todo_id: todos[todo_id]}
+        return {todo_id: return_value}
+#        return {todo_id: todos[todo_id]}
 
 api.add_resource (TodoSimple, '/<string:todo_id>')
+#api.add_resource (TodoSimple, '/<string:todo_id>')
 
 if (__name__ == '__main__'):
     try:

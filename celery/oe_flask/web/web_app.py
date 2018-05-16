3a65af91b65e1a85bc4274854c3ebfef1ba6ed0e
Omer Eisenberg <omereis@yahoo.com>
2018-05-16 17:27:33 -0400

    flask working with index.html

--- celery/oe_flask/web/web_app.py
+++ celery/oe_flask/web/web_app.py
@@ -10,6 +10,14 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'top-secret!'
 
+def get_environ_var (key, default_value):
+    result_value = ""
+    try:
+        result_value = os.environ.get (key)
+    except:
+        result_value = default_value
+    return result_value
+
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
@@ -19,12 +27,46 @@ app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'flask@example.com'
 
 # Celery configuration
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app.config['BROKER_URL']     = get_environ_var ('BROKER_URL', 'redis://localhost:6379/0')
+app.config['RESULT_BACKEND'] = get_environ_var ('RESULT_BACKEND', 'redis://localhost:6379/0')
+
+
+# Initialize extensions
+mail = Mail(app)
+
+try:
+    f = open ('oe_debug.txt', "a+")
+    f.write("app.config['BROKER_URL']: " + app.config['BROKER_URL'])
+    f.close
+except Exception as excp:
+    print ("Error:\n" + str(excp.args))
 
 @app.route('/')
 def hello_world():
     return 'Web App:<br>Flask Dockerized'
+
+
+@app.route('/1', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html', email=session.get('email', ''))
+    email = request.form['email']
+    session['email'] = email
+
+    # send the email
+    msg = Message('Hello from Flask',
+                  recipients=[request.form['email']])
+    msg.body = 'This is a test email sent from a background Celery task.'
+    if request.form['submit'] == 'Send':
+        # send right away
+        send_async_email.delay(msg)
+        flash('Sending email to {0}'.format(email))
+    else:
+        # send in one minute
+        send_async_email.apply_async(args=[msg], countdown=60)
+        flash('An email will be sent to {0} in one minute'.format(email))
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')

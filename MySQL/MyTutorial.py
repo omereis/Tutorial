de546c40a0422377ab6995a7f1885007607a70a3
Omer Eisenberg <omereis@yahoo.com>
2018-06-19 11:25:33 -0400

    reading blob from db to file

--- MySQL/MyTutorial.py
+++ MySQL/MyTutorial.py
@@ -72,13 +72,83 @@ def update_blob(author_id, filename):
         cursor.close()
         conn.close()
 ############################################################################### 
+def save_blob(blob_id ,filename):
+    try:
+        query = "select in_file_blob from tblbumpsinparams where file_id=%S;"
+        conn = connect_bumps()
+        cursor = conn.cursor()
+        cursor.execute(query, (blob_id,))
+        blob_file = cursor.fetchone()[0]
+        write_file(blob_file, filename)
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+############################################################################### 
+def read_blob(author_id, filename):
+    # select photo column of a specific author
+    query = "SELECT photo FROM authors WHERE id = %s"
+ 
+    try:
+        # query blob data form the authors table
+        conn = connect()
+        cursor = conn.cursor()
+        sql = query % author_id
+        cursor.execute(query, (author_id,))
+        record = cursor.fetchone()
+
+        cursor.execute(sql)
+        record = cursor.fetchone()
+#        photo = cursor.fetchone()[0]
+
+        cursor.close()
+        conn.close()
+        conn = connect_bumps()
+        cursor = conn.cursor()
+        sql = "SELECT in_file_name FROM tblbumpsinparams where file_id=%d;" % author_id
+        cursor.execute(sql)
+        record = cursor.fetchone()
+        data = None
+        if record:
+            data = record[0]
+            print("Record fetched: " + data)
+        else:
+            print("Record is NULL")
+
+        sql = "SELECT in_file_blob FROM tblbumpsinparams where file_id=%d;" % author_id
+        cursor.execute(sql)
+        record = cursor.fetchone()
+        if record:
+            data = record[0]
+            write_file(data, filename)
+            print("Record fetched")
+        else:
+            print("Record is NULL")
+
+        # write blob data into a file
+#        write_file(photo, filename)
+ 
+    except Error as e:
+        print(e)
+ 
+    finally:
+        cursor.close()
+        conn.close()
+############################################################################### 
 def read_file(filename):
     with open(filename, 'rb') as f:
         photo = f.read()
     return photo
+############################################################################### 
+def write_file(data, filename):
+    with open(filename, 'wb') as f:
+        f.write(data)
 ############################################################################### 
 if __name__ == '__main__':
 #    query_with_fetchone()
 #    query_with_fetchall()
     update_blob(1, "d:\Omer\Source\Tutorial\MySQL\garth_stein.jpg")
     update_blob(2, "d:\Omer\Source\Tutorial\MySQL\hdf5_job_outline.png")
+    read_blob(2, "d:\\Omer\\Source\\Tutorial\\MySQL\\blob2.png")
+    save_blob(1,'d:\Omer\Source\Tutorial\MySQL\blob2.png')

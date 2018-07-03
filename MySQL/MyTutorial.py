08f692ace811b25085dfc82467a5957a4486689c
Omer Eisenberg <omereis@yahoo.com>
2018-07-03 12:25:32 -0400

    inserting blob to bumps database

--- MySQL/MyTutorial.py
+++ MySQL/MyTutorial.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import MySQLConnection, Error
+import datetime
 ############################################################################### 
 def connect():
     """ Connect to MySQL database """
@@ -20,7 +21,7 @@ def connect_bumps():
     except Error as e:
         print(e)
     return conn
-############################################################################### 
+#------------------------------------------------------------------------------
 def query_with_fetchone():
     try:
         conn = connect()
@@ -37,7 +38,7 @@ def query_with_fetchone():
     finally:
         cursor.close()
         conn.close()
-############################################################################### 
+#------------------------------------------------------------------------------
 def query_with_fetchall():
     try:
         conn = connect()
@@ -60,11 +61,19 @@ def update_blob(author_id, filename):
     # read file
     data = read_file(filename)
     try:
-        query = "UPDATE tblBumpsInParams SET in_file_blob = %s WHERE file_id  = %s"
-        args = (data, author_id)
-        conn = connect_bumps()
+#        conn = connect()
+        conn = mysql.connector.connect(host='localhost', database='python_mysql', user='root', password='masterkey')
         cursor = conn.cursor()
+        query = "select * from authors where id = %s;" % author_id
+        cursor.execute(query)
+        res = cursor.fetchone()
+        print ("Author %d: %s" % (author_id,str(res)))
+        query = "UPDATE authors SET photo = %s WHERE id  = %s"
+#        query = "UPDATE tblBumpsInParams SET in_file_blob = %s WHERE file_id  = %s"
+#        strSql = query % (data, author_id)
+        args = (data, author_id)
         cursor.execute(query, args)
+#        cursor.execute(strSql)
         conn.commit()
     except Error as e:
         print(e)
@@ -135,20 +144,97 @@ def read_blob(author_id, filename):
     finally:
         cursor.close()
         conn.close()
-############################################################################### 
+#------------------------------------------------------------------------------
 def read_file(filename):
     with open(filename, 'rb') as f:
         photo = f.read()
     return photo
-############################################################################### 
+#------------------------------------------------------------------------------
 def write_file(data, filename):
     with open(filename, 'wb') as f:
         f.write(data)
-############################################################################### 
+#------------------------------------------------------------------------------
+def save_list():
+    some_list = ['this', 'that', 'some', 'other', 'thing']
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        results = db.executemany('INSERT INTO tbl (str) VALUES (?)', [(x,) for x in some_list])
+    except Error as e:
+        print(e)
+ 
+    finally:
+        cursor.close()
+        conn.close()
+#------------------------------------------------------------------------------
+def select_author(id):
+    try:
+        conn = mysql.connector.connect(host='localhost', database='python_mysql', user='root', password='masterkey')
+#        conn = mysql.connector.connect(host='ncnr-r9nano', database='bumps_db', user='bumps', password='bumps_dba')
+        cursor = conn.cursor()
+        query = "select * from authors where id=%s;" % id
+#        query = "select * from tbl_authors where id = %s;" % id
+        cursor.execute(query)
+        res = cursor.fetchall()
+        print (("author %s: " % id) + str(res))
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+#------------------------------------------------------------------------------
+TABLE_PARAMS      = "t_bumps_jobs"
+
+FIELD_TOKEN       = 'token'
+FIELD_JOB_ID      = 'job_id'
+FIELD_TIME_START  = 'time_started'
+FIELD_PARAMS      = 'params'
+FIELD_CONTENT     = 'in_file_content'
+FIELD_TIME_ENDED  = 'time_ended'
+FIELD_RES_DIR     = 'result_dir'
+FIELD_RES_CONTENT = 'result_zip'
+#------------------------------------------------------------------------------
+def get_current_time_string():
+    strTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return (strTime)
+#------------------------------------------------------------------------------
+def insert_result (token, job_id, zip_file_name):
+    try:
+        zip_data = read_file (zip_file_name)
+        str_end_time = get_current_time_string()
+        conn = mysql.connector.connect(host='ncnr-r9nano', database='bumps_db', user='bumps', password='bumps_dba')
+        cursor = conn.cursor()
+        strWhere =  " where (%s='%s' and %s=%s);" % (FIELD_TOKEN, str(token), FIELD_JOB_ID, str(job_id))
+        strSql= "update %s set %s='%s' %s;" %  (TABLE_PARAMS, FIELD_TIME_ENDED, str_end_time, strWhere)
+        cursor.execute(strSql)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Token '%s', Job ID %d, end time updated" % (str(token), job_id))
+
+        conn = mysql.connector.connect(host='ncnr-r9nano', database='bumps_db', user='bumps', password='bumps_dba')
+        cursor = conn.cursor()
+        query= "update t_bumps_jobs set result_zip=%s %s;"# %  (TABLE_PARAMS, FIELD_RES_CONTENT, zip_data, strWhere)
+        args = (zip_data, strWhere)
+#        query = "UPDATE authors SET photo = %s WHERE id  = %s"
+        cursor.execute(query, args)
+        conn.commit()
+        print("Token '%s', Job ID %d, blob updated" % (str(token), job_id))
+    except Error as e:
+        print(e)
+    except Exception as e:
+        print(str(e))
+    finally:
+        cursor.close()
+        conn.close()
+#------------------------------------------------------------------------------
 if __name__ == '__main__':
-#    query_with_fetchone()
-#    query_with_fetchall()
-    update_blob(1, "d:\Omer\Source\Tutorial\MySQL\garth_stein.jpg")
-    update_blob(2, "d:\Omer\Source\Tutorial\MySQL\hdf5_job_outline.png")
-    read_blob(2, "d:\\Omer\\Source\\Tutorial\\MySQL\\blob2.png")
-    save_blob(1,'d:\Omer\Source\Tutorial\MySQL\blob2.png')
+    select_author(2)
+#    select_author(12)
+    update_blob(1, "d:\\Omer\\Source\\Tutorial\\MySQL\\garth_stein.jpg")
+    update_blob(2, "d:\\Omer\\Source\\Tutorial\\MySQL\\ccd03d_11.zip")
+    insert_result ('ccd03d', 11, "d:\\Omer\\Source\\Tutorial\\MySQL\\ccd03d_11.zip")
+#    update_blob(2, "d:\\Omer\\Source\\Tutorial\\MySQL\\hdf5_job_outline.png")
+#    read_blob(2, "d:\\Omer\\Source\\Tutorial\\MySQL\\blob2.png")
+#    save_blob(1,'d:\Omer\Source\Tutorial\MySQL\blob2.png')
+#    save_list()
--- celery_test.py
+++ celery_test.py
@@ -1,34 +1,19 @@
-#from __future__ import absolute_import, unicode_literals
-import sys
-
-print("Parsing...using Python" + sys.version) 
-
-try:
-    from celery import Celery
-    print("Celery Imported") 
-except Exception as excp:
-    print ("Error:\n" + str(excp.args)) 
-
-# app = Celery('proj', broker='amqp://rabbit-server', backend='redis://redis-server')
-app = Celery('proj',
-             broker='amqp://rabbit-server',
-             backend='redis://redis-server')
-
-# Optional configuration, see the application user guide.
-app.conf.update(
-    result_expires=3600,
-)
-
-def main():
-    print("Getting app queue data")
-    s = app.control.inspect().stats().keys()
-    print("Stats: " + str(s))
-    for key in s:
-        print("key: " + str(key))
+from celery import Celery
+############################################################################### 
+def list_queues():
+    try:
+        app = Celery('bumps', broker='amqp://rabbit-server', backend='redis://redis-server')
+#        s = app.control.inspect().stats().keys()
+        queues = app.amqp.queues
+        ctrl = app.control
+        qq = ctrl.Mailbox.get_queue()
+        print("Stats: " + str(s))
+        for key in s:
+            print("key: " + str(key))
         s1 = s[key]
-    print("That's it for now\n")
-
+        print("That's it for now\n")
+    except Exception as e:
+        print("Error:\n" + str(e))
+############################################################################### 
 if __name__ == '__main__':
-#    app.start()
-    main()
-    a = 5
+    list_queues()

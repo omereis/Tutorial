cafb2efd8ba4206cd1f485a89d11d3d13b0765bc
Omer Eisenberg <omereis@yahoo.com>
2018-07-12 11:05:46 -0400

    completed flask tutorial part 3

--- flask/discover/app.py
+++ flask/discover/app.py
@@ -1,15 +1,29 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 #------------------------------------------------------------------------------
 # create the application object
 app = Flask(__name__)
+app.secret_key = 'secret_key'
+#------------------------------------------------------------------------------
+def login_required(f):
+    @wraps(f)
+    def wrap (*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Login required')
+            return redirect(url_for('login'))
+    return wrap
 #------------------------------------------------------------------------------
 # use decorators to link the function to a url
 @app.route('/')
+@login_required
 def home():
-    return "Hello, World!"  # return a string
+    return render_template('index.html')  # render a template
 #------------------------------------------------------------------------------
 @app.route('/welcome')
+@login_required
 def welcome():
     return render_template('welcome.html')  # render a template
 #------------------------------------------------------------------------------
@@ -20,8 +34,17 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
+            session['logged_in'] = True
+            flash ('Just logged in :-)')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+#------------------------------------------------------------------------------
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash ('Just logged out :-(')
+    return redirect(url_for('welcome'))
 #------------------------------------------------------------------------------
 # start the server with the 'run()' method
 if __name__ == '__main__':

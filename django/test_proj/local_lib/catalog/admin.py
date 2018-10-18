51751cfcadf97d57304ed3e881736b61e6a4a3b2
Omer Eisenberg <omereis@yahoo.com>
2018-10-18 14:16:55 -0400

    django tutorial with inline

--- django/test_proj/local_lib/catalog/admin.py
+++ django/test_proj/local_lib/catalog/admin.py
@@ -13,19 +13,27 @@ from catalog.models import Author, Genre, Book, BookInstance
 admin.site.register(Genre)
 #admin.site.register(BookInstance)
 #------------------------------------------------------------------------------
+class BookInline(admin.TabularInline):
+    model = Book
+#------------------------------------------------------------------------------
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-admin.site.register(Author, AuthorAdmin)
+    inlines = [BookInline]
+#------------------------------------------------------------------------------
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 #------------------------------------------------------------------------------
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
-
+    inlines = [BookInstanceInline]
+#------------------------------------------------------------------------------
 @admin.register(BookInstance)
 class BookInstanceAdmin (admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'imprint', 'due_back', 'id')
 
     fieldsets = (
         ('Main',{

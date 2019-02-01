73849692cbecedd0e0e3367a877dbcd70b2c3674
omereis <omereis@yahoo.com>
2019-02-01 10:02:50 -0500

    new baseline

--- flask/greenberg/microblog/app/routes.py
+++ flask/greenberg/microblog/app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, flash, redirect, url_for
 from app import app
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
-
+from oe_debug import print_debug
 #------------------------------------------------------------------------------
 @app.route('/')
 @app.route('/index')
@@ -18,14 +18,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-
 #------------------------------------------------------------------------------
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='Sign In', form=form)
-
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)

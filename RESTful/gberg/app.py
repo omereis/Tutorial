8b04b00650e22cdbfb3ee364dc70a5e236dc545e
omereis <omereis@yahoo.com>
2019-02-19 16:47:43 -0500

    adding put/update

--- RESTful/gberg/app.py
+++ RESTful/gberg/app.py
@@ -1,4 +1,6 @@
 #!flask/bin/python
+#https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+#https://blog.miguelgrinberg.com/post/writing-a-javascript-rest-client
 
 from flask import Flask, jsonify, render_template, redirect, url_for, request, abort, make_response
 
@@ -30,6 +32,13 @@ tasks = [
     }
 ]
 
+def next_task_id ():
+    id=0
+    for n in range (len(tasks)):
+        if 'id' in tasks[n].keys():
+            id = max(id, tasks[n]['id'])
+    return id + 1
+
 from oe_debug import print_debug
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
@@ -50,25 +59,59 @@ def not_found(error):
 
 import datetime, json
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    msg = request.data
+def request_data_to_json(msg):
     jsn = msg.decode('utf-8')
-    print_debug(('jsn: {}').format(jsn))
     s=jsn[jsn.find('{')+1:jsn.find('}')]
     s_split=s.split(':')
     jsn_src = '{"'+s_split[0]+'":"'+s_split[1]+'"}'
-    print_debug("jsn_src: " + str(jsn_src))
     j_data = json.loads(jsn_src)
-    print_debug("j_data: " + str(j_data))
+    return j_data
+
+def complete_record(j_data):
+    if ('id' not in j_data.keys()):
+        j_data['id'] = tasks[-1]['id'] + 1
+    if ('title' not in j_data.keys()):
+        j_data['title'] = ""
+    if ('description' not in j_data.keys()):
+        j_data['description'] = ""
+    if ('done' not in j_data.keys()):
+        j_data['done'] = False
+    return j_data
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    j_data = request_data_to_json (request.data)
     task = {
         'id': tasks[-1]['id'] + 1,
         'title': j_data['title'],
         'description': "",
         'done': False
     }
+    j_data = complete_record(j_data)
     tasks.append(task)
     return jsonify({'task': task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    print_debug('task_id: ' + str(task_id))
+    task = [task for task in tasks if task['id'] == task_id]
+    print_debug('task: ' + str(task))
+    if len(task) == 0:
+        abort(404)
+    #if not request.json:
+        #abort(400)
+    print_debug('request data: ' + str(request.data))
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)

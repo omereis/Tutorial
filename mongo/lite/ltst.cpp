cf154e1e57b2887b2c4868d72209d2110553c9d3
omereis <omereis@yahoo.com>
2019-03-12 17:07:52 -0400

    completed file test, started sqlite3

---
+++ mongo/lite/ltst.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sqlite3.h> 
+//-----------------------------------------------------------------------------
+bool open_database (char *szDB, sqlite3 **db, int argc, char *argv[]);
+void create_blobs_table()
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	char *szDB = "db_blobs.sqlite";
+	int rc;
+
+	if (open_database (szDB, &db, argc, argv)) {
+		printf ("Database opened\n");
+		create_blobs_table();
+		sqlite3_close(db);
+		printf ("Database Closed\n");
+	}
+/*
+	rc = sqlite3_open("test.db", &db);
+
+	if (rc) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return(0);
+	}
+	else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	sqlite3_close(db);
+*/
+}
+//-----------------------------------------------------------------------------
+bool open_database (char *szDB, sqlite3 **db, int argc, char *argv[])
+{
+	int rc = sqlite3_open(szDB, db);
+	return (rc != 0);
+}
+//-----------------------------------------------------------------------------
+void create_blobs_table()
+{
+	const string strTableName = "tbl_blob";
+	
+}
+//-----------------------------------------------------------------------------
+

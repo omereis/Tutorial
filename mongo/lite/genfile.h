0f45f31cc85fc4cb0208a842c9ed497781084015
omereis <omereis@yahoo.com>
2019-03-14 16:08:00 -0400

    inserting to sqlite3

---
+++ mongo/lite/genfile.h
@@ -0,0 +1,9 @@
+#ifndef	_GENFILE_H
+#define	_GINFILE_H
+
+#include <string>
+#include "fm.h"
+//-----------------------------------------------------------------------------
+void generate_file (char szName[], struct FileMaker *pfm);
+
+#endif

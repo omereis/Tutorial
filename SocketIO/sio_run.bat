7940a6d609b607ea47ea802fbae2eb661c0fb468
omereis <omereis@yahoo.com>
2019-04-22 17:32:18 -0400

    added SocketIO server

--- SocketIO/sio_run.bat
+++ SocketIO/sio_run.bat
@@ -1,5 +1,5 @@
 docker container rm -f sio
-docker build --rm -t sio -f sio.dockerfile .
-docker run -d -it --name sio -p 4000:4000 -p 8765:8765 sio
+docker build --rm -t sio5 -f sio5.dockerfile .
+docker run -d -it --name sio5 -p 5000:5000 sio5
 docker exec -i -t sio bash
 

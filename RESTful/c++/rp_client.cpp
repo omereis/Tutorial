d6cad96d8682706e12a11c0be180cfd193f8af10
Omer Eisenberg <omereis@yahoo.com>
2021-01-31 14:58:40 +0200

    red pitaya tcp client & server

---
+++ RESTful/c++/rp_client.cpp
@@ -0,0 +1,84 @@
+// Client side C/C++ program to demonstrate Socket programming 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#define PORT 5500
+
+#include <string>
+#include <stdlib.h>
+#include "comm.h"
+
+using namespace std;
+
+//-----------------------------------------------------------------------------
+int SafeReadCliPort (const char *szPort)
+{
+	int nPort;
+
+	try {
+		nPort = atoi(szPort);
+	}
+	catch (const std::exception &e){
+		nPort = DEFAULT_PORT;
+	}
+	return (nPort);
+}
+//-----------------------------------------------------------------------------
+bool GetCliAddressPort (int argc, char const *argv[], int &nPort, string &strAddress, string &strErr)
+{
+	bool f;
+		
+	try {
+		nPort = DEFAULT_PORT;
+		strAddress = DEFAULT_ADDRESS;
+		if (argc > 1)
+			nPort = SafeReadCliPort (argv[1]);
+		if (argc > 2)
+			strAddress = argv[2];
+		f = true;
+	}
+	catch (exception &e){
+		nPort = DEFAULT_PORT;
+		strAddress = DEFAULT_ADDRESS;
+		strErr = e.what();
+		f = false;
+	}
+	return (f);
+}
+
+int main(int argc, char const *argv[]) 
+{ 
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+	const char *hello = "Hello from client"; 
+	char buffer[1024] = {0}; 
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	} 
+	send(sock , hello , strlen(hello) , 0 ); 
+	printf("Hello message sent\n"); 
+	valread = read( sock , buffer, 1024); 
+	printf("%s\n",buffer ); 
+	return 0; 
+} 

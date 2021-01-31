92e1d7e7b146c586dbb107aefb2d2e0ef7991d7c
Omer Eisenberg <omereis@yahoo.com>
2021-01-31 15:04:15 +0200

    remoed spaces, empty lines

--- RESTful/c++/rp_client.cpp
+++ RESTful/c++/rp_client.cpp
@@ -1,9 +1,10 @@
 // Client side C/C++ program to demonstrate Socket programming 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
 #define PORT 5500
 
 #include <string>
@@ -47,38 +48,32 @@ bool GetCliAddressPort (int argc, char const *argv[], int &nPort, string &strAdd
 	}
 	return (f);
 }
+//-----------------------------------------------------------------------------
+int main(int argc, char const *argv[])
+{
+	int sock = 0, valread;
+	struct sockaddr_in serv_addr;
+	const char *hello = "Hello from client";
+	char buffer[1024] = {0};
 
-int main(int argc, char const *argv[]) 
-{ 
-	int sock = 0, valread; 
-	struct sockaddr_in serv_addr; 
-	const char *hello = "Hello from client"; 
-	char buffer[1024] = {0}; 
-
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-	{ 
-		printf("\n Socket creation error \n"); 
-		return -1; 
-	} 
-
-	serv_addr.sin_family = AF_INET; 
-	serv_addr.sin_port = htons(PORT); 
-	
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
 	// Convert IPv4 and IPv6 addresses from text to binary form 
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-	{ 
-		printf("\nInvalid address/ Address not supported \n"); 
-		return -1; 
-	} 
-
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-	{ 
-		printf("\nConnection Failed \n"); 
-		return -1; 
-	} 
-	send(sock , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	valread = read( sock , buffer, 1024); 
-	printf("%s\n",buffer ); 
-	return 0; 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <=0 ) { 
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(sock , hello , strlen(hello) , 0 );
+	printf("Hello message sent\n");
+	valread = read( sock , buffer, 1024);
+	printf("%s\n",buffer );
+	return 0;
 } 

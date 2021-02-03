c4e98ba547b55035aa00d18337225a28d72e3c87
Omer Eisenberg <omereis@yahoo.com>
2021-02-03 11:55:06 +0200

    using port number from command line, using GetCliPort() function

--- RESTful/c++/rp_server.cpp
+++ RESTful/c++/rp_server.cpp
@@ -22,6 +22,7 @@ int GetCliPort (int argc, char const *argv[])
 	return (nPort);
 }
 //-----------------------------------------------------------------------------
+
 int OpenSocket (int nPort)
 {
 	struct sockaddr_in address; 
@@ -56,6 +57,7 @@ int OpenSocket (int nPort)
 	return (nServerFd);
 }
 //-----------------------------------------------------------------------------
+
 int main(int argc, char const *argv[])
 {
 	int server_fd, new_socket, valread;
@@ -64,8 +66,11 @@ int main(int argc, char const *argv[])
 	int addrlen = sizeof(address);
 	char buffer[1024] = {0};
 	const char *hello = "Hello from server";
-	int nPort = DEFAULT_PORT;
+	int nPort;// = DEFAULT_PORT;
 
+	nPort = GetCliPort (argc, argv);
+	server_fd = OpenSocket (nPort);
+/*
 	// Creating socket file descriptor
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
 		perror("socket failed");
@@ -84,18 +89,18 @@ int main(int argc, char const *argv[])
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-/*
-*/
+
 	fprintf (stderr, "Before listen\n");
 	if (listen(server_fd, 3) < 0) { 
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
+*/
 	char szHello[1024];
 	//for (int n=0 ; n < 5 ; n++) {
 	int n=1;
 	do {
-		fprintf (stderr, "Before accept\n");
+		fprintf (stderr, "Waiting at port %d\n", nPort);
 		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0){ 
 			perror("accept");
 			exit(EXIT_FAILURE);

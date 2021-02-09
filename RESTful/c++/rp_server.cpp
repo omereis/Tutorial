c55502bcdbc9dadf23f84b419721c2e1e3f04eed
Omer Eisenberg <omereis@yahoo.com>
2021-02-09 06:48:14 +0200

    removed commented statements

--- RESTful/c++/rp_server.cpp
+++ RESTful/c++/rp_server.cpp
@@ -70,34 +70,8 @@ int main(int argc, char const *argv[])
 
 	nPort = GetCliPort (argc, argv);
 	server_fd = OpenSocket (nPort);
-/*
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-	// Forcefully attaching socket to the port 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(nPort);
-	// Forcefully attaching socket to the port
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-
-	fprintf (stderr, "Before listen\n");
-	if (listen(server_fd, 3) < 0) { 
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-*/
 	char szHello[1024];
-	//for (int n=0 ; n < 5 ; n++) {
+
 	int n=1;
 	do {
 		fprintf (stderr, "Waiting at port %d\n", nPort);
